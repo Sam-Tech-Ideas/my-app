96e8768b3daccdb9ad8122f2b1da798954250d94
Sam-tech-ideas <agyemangsamuel235@gmail.com>
2024-10-11 22:30:00 +0000

    hubtel ui

--- App.js
+++ App.js
@@ -1,20 +1,131 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, Image, TextInput, TouchableOpacity } from "react-native";
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView className="bg-gray-100  h-full">
+      <View className="p-4">
+        {/***Topbar start */}
+        <View className="flex-row justify-between items-center">
+          {/**** child one start*/}
+          <View className="bg-white rounded-full  w-10 p-2 ">
+            <FontAwesome name="bell-o" size={24} color="black" />
+          </View>
+          {/**child one end */}
+          {/**** child two start*/}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          <View className="h-10 w-40">
+            <Image
+              source={require("./assets/logo.png")}
+              className="w-full h-full object-cover"
+            />
+          </View>
+          {/**child two end */}
+          {/**** child three start*/}
+          <View className="bg-gray-400 p-2 rounded-full w-10">
+            <Fontisto name="male" size={24} color="white" />
+          </View>
+          {/**child three end */}
+        </View>
+        {/**Topbar end */}
+
+        {/***search start */}
+        <View className="bg-white mx-auto my-6 flex-row items-center justify-between w-[360] shadow-lg rounded p-4">
+          <TextInput
+            className="font-bold"
+            placeholder="Search Hubtel"
+            keyboardType="numeric"
+          />
+          <Ionicons name="search-outline" size={24} color="black" />
+        </View>
+
+        {/**search end */}
+
+        {/**services start */}
+
+        <View className="flex-row items-center justify-center gap-2 my-2 flex-wrap">
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="h-28 gap-y-2 w-28 rounded-xl bg-white flex-col  justify-center items-center">
+            <View>
+              <MaterialCommunityIcons name="cash" size={24} color="black" />
+            </View>
+            <View>
+              <Text>Send Money</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {/**services end */}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
